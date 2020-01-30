9166a01531ee76279a838421568e417232c261d0
gavril.hasmasan <gavril.hasmasan@pitechnologies.ro>
2020-01-30 11:27:04 +0200

    almost final

--- js.js
+++ js.js
@@ -5,68 +5,40 @@ new Vue({
         yourLife: 100,
         monsterLife: 100,
         hits: [],
-        gameOver: false
     },
     methods: {
         attack() {
-            function randomDamage() {
-                return Math.floor(Math.random() * 20);
-            }
-            const damage1 = randomDamage();
-            const damage2 = randomDamage();
-            this.hits.unshift({ "Monster hits player for": damage1 });
-            this.hits.unshift({ "Player hits monster for": damage2 });
-            console.log(this.hits);
-            let temp1 = 0;
-            let temp2 = 0;
-            if (this.yourLife && this.monsterLife > 0) {
-                const cl1 = setInterval(() => {
-                    this.yourLife -= 1;
-                    temp1 += 1;
-                    if (temp1 === damage1 || this.monsterLife === 0 || this.yourLife === 0) {
-                        clearInterval(cl1);
-                    }
-                    if (this.yourLife === 0) {
-                        this.gameOver = true;
-                        this.started = false;
-                        this.hits = [];
-                    }
-
-                }, 20);
-
-                const cl2 = setInterval(() => {
-                    this.monsterLife -= 1;
-                    temp2 += 1;
-                    if (temp2 === damage2 || this.monsterLife === 0 || this.yourLife === 0) {
-                        clearInterval(cl2);
-                    }
-                    if (this.monsterLife === 0) {
-                        this.gameOver = true;
-                        this.started = false;
-                        this.hits = [];
-                    }
-                }, 20);
+            if (this.yourLife > 0 && this.monsterLife > 0) {
+                function randomDamage() {
+                    return Math.floor(Math.random() * 20);
+                }
+                const damage1 = randomDamage();
+                const damage2 = randomDamage();
+                this.hits.unshift({ "Monster hits player for": damage1 });
+                this.hits.unshift({ "Player hits monster for": damage2 });
+                console.log(this.hits);
+                if (this.yourLife && this.monsterLife > 0) {
+                    this.yourLife -= damage1;
+                    this.monsterLife -= damage2;
+                }
+                if (this.yourLife <= 0 || this.monsterLife <= 0) {
+                    alert("game over");
+                    this.started = false;
+                    this.hits = [];
+                    this.yourLife = 100;
+                    this.monsterLife = 100;
+                }
             }
         },
         giveUp() {
             this.started = false;
             this.yourLife = 100;
             this.monsterLife = 100;
-            gameOver = true;
-            this.started = false;
             this.hits = [];
         },
         heal() {
-            const plus = 10;
-            let temp = 0;
-            const cl1 = setInterval(() => {
-                this.yourLife += 1;
-                temp += 1;
-                if (temp === plus) {
-                    clearInterval(cl1);
-                }
 
-            }, 20);
+            this.yourLife += 10;
         }
     },
     computed: {
@@ -83,15 +55,10 @@ new Vue({
                 width: this.monsterLife + "%"
             }
         }
+
     },
     watch: {
-        gameOver() {
-            this.yourLife = 100;
-            this.monsterLife = 100;
-        },
-        started() {
-            this.yourLife = 100;
-            this.monsterLife = 100; 
-        }
+
+
     }
 });
